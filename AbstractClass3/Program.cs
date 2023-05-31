2edf883ccc392ed293fe213ae0df2e647a55039a
beverley-dick <beverleydick11@gmail.com>
2023-05-31 11:27:59 +0300

    Add project files.

---
+++ AbstractClass3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AbstractClass3
+{
+   abstract class Animal
+    {
+        protected string name;
+       //abstract method
+       public abstract string Name
+        {
+            get;
+            set;
+        }
+    }
+
+    //inheriting from abstract class
+    class Dog : Animal
+    {
+        //provide implimantation of abstract method
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            //create an object of Dog class
+            Dog obj = new Dog();
+            obj.Name = "Tom";
+            Console.WriteLine("Name: " + obj.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
